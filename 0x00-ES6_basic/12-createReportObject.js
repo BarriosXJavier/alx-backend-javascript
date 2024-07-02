e9172ff7e9df54a779a7c571f1285574781031f6
David Muriithi <muriithid05@gmail.com>
2024-07-02 22:10:24 +0300

    refactor: Update createReportObject to use object shorthand syntax and arrow function for getNumberOfDepartments

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,19 +1,8 @@
 export default function createReportObject(employeesList) {
-	// Extract department names from employeesList keys
-	const departments = Object.keys(employeesList);
-
-	// Function to get number of departments
-	const getNumberOfDepartments = () => departments.length;
-
-	// Constructing allEmployees object using spread syntax
-	const allEmployees = {};
-	for (let department of departments) {
-		allEmployees[department] = [...employeesList[department]];
-	}
-
-	// Return the report object
 	return {
-		allEmployees,
-		getNumberOfDepartments,
+		allEmployees: employeesList,
+		getNumberOfDepartments(employeesList) {
+			return Object.keys(employeesList).length;
+		},
 	};
 }
