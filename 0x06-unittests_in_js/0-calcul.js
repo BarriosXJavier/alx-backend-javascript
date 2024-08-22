c1614d0afceb6b1d580dce89b1d3fcd9faa3da5c
David Muriithi <muriithid05@gmail.com>
2024-08-23 01:28:23 +0300

    new file:   0x06-unittests_in_js/0-calcul.js 	new file:   0x06-unittests_in_js/0-calcul.test.js

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+// Calc function.
+
+function calculateNumber(a, b) {
+    firstNum = Math.round(a);
+    secondNum = Math.round(b);
+    return firstNum + secondNum;
+}
+
+module.exports = calculateNumber;
